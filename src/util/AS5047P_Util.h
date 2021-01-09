98d7fe2a911bc454bb3e455efe90119a6b7682b8
Jonas Merkle [JJM] <jonas@jjm.one>
2021-01-09 21:32:30 +0100

    fixed parity handling

--- src/util/AS5047P_Util.h
+++ src/util/AS5047P_Util.h
@@ -10,35 +10,29 @@
 namespace AS5047P_Util {
 
     /**
-     * Checks if a data package (where the parity bit isn't set yet (parity bit is 0)) has an even number of ones or not.
+     * Checks if a data package has an even number of ones or not.
      * @param data The data package.
-     * @return True if the number of ones in the data package (including the parity bit!!! (which must not be set yet)) is even, else false.
+     * @return True if the number of ones in the data package is even, else false.
      */
-    static inline bool isEvenParityBitNoSet(uint16_t data) {
-        uint16_t shift = data >> 8;
-        data ^= data >> 8;      // example for 8-bir (this line scales it up to 16 bit)
-        data ^= data >> 4;      // ( a b c d e f g h ) xor ( 0 0 0 0 a b c d ) = ( a b c d ae bf cg dh )
-        data ^= data >> 2;      // ( a b c d ae bf cg dh ) xor ( 0 0 a b c d ae bf ) = ( a b ac bd ace bdf aceg bdfh )
-        data ^= data >> 1;      // ( a b ac bd ace bdf aceg bdfh ) xor ( 0 a b ac bd ace bdf aceg ) = ( a ab abc abcd abcde abcdef abcdefg abcdefgh )
-        return (data) & 1;      // if lsb of data is 0 -> data is even. if lsb of data is 1 -> data is odd. Don't invert (~data) result because parity bit gets added afterwards.
-    }
+    static inline bool hasEvenNoOfBits(uint16_t data) {
 
-    /**
-     * Checks if a data package (where the parity bit is set) has an even number of ones or not.
-     * @param data The data package.
-     * @return True if the number of ones in the data package (including the parity bit!!! (which is already set yet)) is even, else false.
-     */
-    static inline bool isEvenParityBitSet(uint16_t data) {
-        return ~isEvenParityBitNoSet(data);
+        data ^= data >> 8;              // example for 8-bir (this line scales it up to 16 bit)
+        data ^= data >> 4;              // ( a b c d e f g h ) xor ( 0 0 0 0 a b c d ) = ( a b c d ae bf cg dh )
+        data ^= data >> 2;              // ( a b c d ae bf cg dh ) xor ( 0 0 a b c d ae bf ) = ( a b ac bd ace bdf aceg bdfh )
+        data ^= data >> 1;              // ( a b ac bd ace bdf aceg bdfh ) xor ( 0 a b ac bd ace bdf aceg ) = ( a ab abc abcd abcde abcdef abcdefg abcdefgh )
+        return (bool) ((~data) & 1);    // if lsb of data is 0 -> data is even. if lsb of data is 1 -> data is odd. 
+
     }
 
     /**
      * Checks if the parity information in a data package is correct.
-     * @param data The data package.
+     * @param rawData The raw data package.
      * @return True if the parity information is correct, else false.
      */
-    static inline bool parityCheck(uint16_t data) {
-        return ((data & (1 << 15)) && isEvenParityBitSet(data));
+    static inline bool parityCheck(uint16_t rawData) {
+
+        return ((bool) ~(rawData & (1 << 15)) && (bool) hasEvenNoOfBits(rawData & 0x7FFF));
+
     }
 }
 
