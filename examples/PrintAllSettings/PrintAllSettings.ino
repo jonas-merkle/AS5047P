c1e1fe94ab588a6095797875b8c61dad9d758e95
Jonas Merkle [JJM] (jonas@jjm.one) <jonas@jjm.one>
2022-11-20 12:20:29 +0100

    examples fixed

--- examples/PrintAllSettings/PrintAllSettings.ino
+++ examples/PrintAllSettings/PrintAllSettings.ino
@@ -2,8 +2,8 @@
  * @file PrintAllSettings.ino
  * @author Jonas Merkle [JJM] (jonas@jjm.one)
  * @brief This is a example prints out the settings of the AS5047P sensor.
- * @version 2.1.5
- * @date 2021-04-10
+ * @version 2.2.0
+ * @date 2022-11-20
  * 
  * @copyright Copyright (c) 2021 Jonas Merkle. This project is released under the GPL-3.0 License License.
  * 
