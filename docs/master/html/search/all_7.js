015effbcafb89ebea72cc8a7fe29714a9958b4a7
jonas-merkle <jonas@jjm.one>
2024-10-19 21:52:46 +0000

    Deploying to gh-pages from  @ 52fcf7620ba9608f33bcc9324fe134d1c388d1fc 🚀

--- docs/master/html/search/all_7.js
+++ docs/master/html/search/all_7.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['mag_5fdata_5ft_0',['MAG_data_t',['../union_a_s5047_p___types_1_1_m_a_g__t_1_1_m_a_g__data__t.html',1,'AS5047P_Types::MAG_t']]],
-  ['mag_5ft_1',['MAG_t',['../class_a_s5047_p___types_1_1_m_a_g__t.html',1,'AS5047P_Types::MAG_t'],['../class_a_s5047_p___types_1_1_m_a_g__t.html#a7b2fa59846143640b5a2bf67433d393b',1,'AS5047P_Types::MAG_t::MAG_t()'],['../class_a_s5047_p___types_1_1_m_a_g__t.html#ad731ba3f20a409be35c926bf1d278a21',1,'AS5047P_Types::MAG_t::MAG_t(uint16_t raw)']]],
+  ['mag_5ft_1',['mag_t',['../class_a_s5047_p___types_1_1_m_a_g__t.html',1,'AS5047P_Types::MAG_t'],['../class_a_s5047_p___types_1_1_m_a_g__t.html#a7b2fa59846143640b5a2bf67433d393b',1,'AS5047P_Types::MAG_t::MAG_t()'],['../class_a_s5047_p___types_1_1_m_a_g__t.html#ad731ba3f20a409be35c926bf1d278a21',1,'AS5047P_Types::MAG_t::MAG_t(uint16_t raw)']]],
   ['mag_5fvalues_5ft_2',['MAG_values_t',['../union_a_s5047_p___types_1_1_m_a_g__t_1_1_m_a_g__data__t.html#a8398a480b1a14b4c46b1633a4cf98076',1,'AS5047P_Types::MAG_t::MAG_data_t']]]
 ];
--- docs/master/html/search/all_9.js
+++ docs/master/html/search/all_9.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['prog_5fdata_5ft_0',['PROG_data_t',['../union_a_s5047_p___types_1_1_p_r_o_g__t_1_1_p_r_o_g__data__t.html',1,'AS5047P_Types::PROG_t']]],
-  ['prog_5ft_1',['PROG_t',['../class_a_s5047_p___types_1_1_p_r_o_g__t.html',1,'AS5047P_Types::PROG_t'],['../class_a_s5047_p___types_1_1_p_r_o_g__t.html#a6ca1b9ef647e48f7ff71d1671d578a29',1,'AS5047P_Types::PROG_t::PROG_t()'],['../class_a_s5047_p___types_1_1_p_r_o_g__t.html#a2ba4227411264d7a6672cf8a44dc6787',1,'AS5047P_Types::PROG_t::PROG_t(uint16_t raw)']]],
+  ['prog_5ft_1',['prog_t',['../class_a_s5047_p___types_1_1_p_r_o_g__t.html',1,'AS5047P_Types::PROG_t'],['../class_a_s5047_p___types_1_1_p_r_o_g__t.html#a6ca1b9ef647e48f7ff71d1671d578a29',1,'AS5047P_Types::PROG_t::PROG_t()'],['../class_a_s5047_p___types_1_1_p_r_o_g__t.html#a2ba4227411264d7a6672cf8a44dc6787',1,'AS5047P_Types::PROG_t::PROG_t(uint16_t raw)']]],
   ['prog_5fvalues_5ft_2',['PROG_values_t',['../union_a_s5047_p___types_1_1_p_r_o_g__t_1_1_p_r_o_g__data__t.html#a0bba4a660ee8ea5bd98b67ffdc114991',1,'AS5047P_Types::PROG_t::PROG_data_t']]]
 ];
