306f272d0a23ff0dc206f1c1ff0c75edbb460bd5
jonas-merkle <jonas@jjm.one>
2024-10-19 21:39:32 +0000

    Deploying to gh-pages from  @ 08eadff919d63d3e35b29ff62aece18ec916c7b2 🚀

--- docs/develop/html/search/all_4.js
+++ docs/develop/html/search/all_4.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['errfl_5fdata_5ft_0',['ERRFL_data_t',['../union_a_s5047_p___types_1_1_e_r_r_f_l__t_1_1_e_r_r_f_l__data__t.html',1,'AS5047P_Types::ERRFL_t']]],
-  ['errfl_5ft_1',['ERRFL_t',['../class_a_s5047_p___types_1_1_e_r_r_f_l__t.html',1,'AS5047P_Types::ERRFL_t'],['../class_a_s5047_p___types_1_1_e_r_r_f_l__t.html#a130ff6d7df18bc03d3bc6feac1b64964',1,'AS5047P_Types::ERRFL_t::ERRFL_t()'],['../class_a_s5047_p___types_1_1_e_r_r_f_l__t.html#a1e4f68e1bf20e7b6f3b64ea10be2c60f',1,'AS5047P_Types::ERRFL_t::ERRFL_t(uint16_t raw)']]],
+  ['errfl_5ft_1',['errfl_t',['../class_a_s5047_p___types_1_1_e_r_r_f_l__t.html',1,'AS5047P_Types::ERRFL_t'],['../class_a_s5047_p___types_1_1_e_r_r_f_l__t.html#a130ff6d7df18bc03d3bc6feac1b64964',1,'AS5047P_Types::ERRFL_t::ERRFL_t()'],['../class_a_s5047_p___types_1_1_e_r_r_f_l__t.html#a1e4f68e1bf20e7b6f3b64ea10be2c60f',1,'AS5047P_Types::ERRFL_t::ERRFL_t(uint16_t raw)']]],
   ['errfl_5fvalues_5ft_2',['ERRFL_values_t',['../union_a_s5047_p___types_1_1_e_r_r_f_l__t_1_1_e_r_r_f_l__data__t.html#a7b869f396a65f8423980c6808aaba0ca',1,'AS5047P_Types::ERRFL_t::ERRFL_data_t']]],
   ['error_5fnames_3',['ERROR_Names',['../namespace_a_s5047_p___types.html#a2143ebf9da2e0cd376fadf1f23f11f6b',1,'AS5047P_Types']]],
-  ['error_5ft_4',['ERROR_t',['../class_a_s5047_p___types_1_1_e_r_r_o_r__t.html',1,'AS5047P_Types::ERROR_t'],['../class_a_s5047_p___types_1_1_e_r_r_o_r__t.html#af837a80e0d7dea750d249879af85695c',1,'AS5047P_Types::ERROR_t::ERROR_t()']]]
+  ['error_5ft_4',['error_t',['../class_a_s5047_p___types_1_1_e_r_r_o_r__t.html',1,'AS5047P_Types::ERROR_t'],['../class_a_s5047_p___types_1_1_e_r_r_o_r__t.html#af837a80e0d7dea750d249879af85695c',1,'AS5047P_Types::ERROR_t::ERROR_t()']]]
 ];
