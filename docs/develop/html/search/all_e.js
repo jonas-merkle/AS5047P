578688f80b216c4082b32fe3e03599d9d993ef38
jonas-merkle <jonas@jjm.one>
2025-10-07 16:23:02 +0000

    Deploying to gh-pages from  @ b1ea8992006fde560d43bff1ec3c0c87924c1053 🚀

--- docs/develop/html/search/all_e.js
+++ docs/develop/html/search/all_e.js
@@ -1,10 +1,10 @@
 var searchData=
 [
   ['write_0',['write',['../class_a_s5047_p___com_backend_1_1_a_s5047_p___s_p_i.html#ae1d41dbbd3ceaa40e51631438497ddd4',1,'AS5047P_ComBackend::AS5047P_SPI']]],
-  ['write_5fprog_1',['write_PROG',['../class_a_s5047_p.html#aa99d848826dca53ab4417607e12e4529',1,'AS5047P']]],
-  ['write_5fsettings1_2',['write_SETTINGS1',['../class_a_s5047_p.html#a06fb43106c05cbccc2bba67cda9b751a',1,'AS5047P']]],
-  ['write_5fsettings2_3',['write_SETTINGS2',['../class_a_s5047_p.html#addfdb571b06059c4242d83d389edcc68',1,'AS5047P']]],
-  ['write_5fzposl_4',['write_ZPOSL',['../class_a_s5047_p.html#a368a0c7eadde61697c8e06164d271203',1,'AS5047P']]],
-  ['write_5fzposm_5',['write_ZPOSM',['../class_a_s5047_p.html#a6ff157577f161dbb4263e1e54cb0d15e',1,'AS5047P']]],
-  ['writereg_6',['writeReg',['../class_a_s5047_p.html#a134a9df154b185e9942d284ad1909387',1,'AS5047P']]]
+  ['write_5fprog_1',['write_PROG',['../class_a_s5047_p.html#a676e2576f6865c7c423dd08262080065',1,'AS5047P']]],
+  ['write_5fsettings1_2',['write_SETTINGS1',['../class_a_s5047_p.html#a009c40f7e9cfb96ca71802e78d14f836',1,'AS5047P']]],
+  ['write_5fsettings2_3',['write_SETTINGS2',['../class_a_s5047_p.html#ab54d72a6d19326f91a91e0d8afc4d9bf',1,'AS5047P']]],
+  ['write_5fzposl_4',['write_ZPOSL',['../class_a_s5047_p.html#a0c905545901bc86e28332408da9681bf',1,'AS5047P']]],
+  ['write_5fzposm_5',['write_ZPOSM',['../class_a_s5047_p.html#a3166e5e33f698df28c15842329ab1a02',1,'AS5047P']]],
+  ['writereg_6',['writeReg',['../class_a_s5047_p.html#a6b5efa3ef54b2e360dd0e973672a273a',1,'AS5047P']]]
 ];
